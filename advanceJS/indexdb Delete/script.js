d299a4df3ef0d2bce40c045c72e92fdb8cb3f76f
slavik <116015277+masihSlavik@users.noreply.github.com>
2022-12-26 12:30:01 -0800

    Add files via upload

---
+++ advanceJS/indexdb Delete/script.js
@@ -0,0 +1,65 @@
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const submit = document.getElementById('submit')
+let t = document.getElementById('t')
+let db =null
+let database = indexedDB.open('schools' , 1)
+
+database.onupgradeneeded = ()=>{
+    db = database.result
+if(!db.objectStoreNames.contains("user")){
+    db.createObjectStore("user" , {keyPath:"id"})
+}
+
+
+}
+database.onsuccess=()=>{
+    db = database.result
+    getuser()
+   
+}
+submit.addEventListener('click' , ()=>{
+    db = database.result
+    let user = {id:Math.floor(Math.random() *1000) ,  name:name.value , password:password.value}
+    let transaction = db.transaction("user" , "readwrite")
+    let store = transaction.objectStore("user")
+    store.add(user)
+    getuser()
+})
+
+function getuser (){
+    let transaction = db.transaction("user" , "readwrite")
+    let store = transaction.objectStore("user")
+    let request = store.getAll()
+    request.addEventListener('success' , (e)=>{
+        t.innerHTML = `
+        <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>password</th>
+        <th>acthin</th>
+        
+            </tr>`
+        let  users = e.target.result
+        users.map(user =>{
+            t.innerHTML += `    <tr>
+            <th>${user.id}</th>
+            <th>${user.name}</th>
+            <th>${user.password}</th>
+            <th><a href="#" onclick="deleted(${user.id})">delet</a></th>
+            
+                </tr>`
+        })
+
+    })
+
+}
+function deleted(id){
+    event.preventDefault()
+    db = database.result
+    let transaction = db.transaction('user' , 'readwrite')
+    let store = transaction.objectStore("user")
+    let request = store.delete(id)
+        console.log(request)
+        getuser()
+}
